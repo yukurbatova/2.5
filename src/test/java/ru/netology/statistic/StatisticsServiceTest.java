c39ca3d6fced347999806d0867b6d3cbbfd6db07
Yuliya Kurbatova <j53781551@gmail.com>
2021-01-28 22:42:39 +0300

    Test findMax02 added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,4 +18,16 @@ class StatisticsServiceTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void findMax02() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 12, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
 }
